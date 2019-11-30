8e92951ac21ec0ac779961900945cd280b9cd6fb
r-plus <anasthasia.r@gmail.com>
2019-11-30 17:21:05 +0900

    unlock after complete dispatch; value property use lock

--- Sources/SimpleRx/BehaviorRelay.swift
+++ Sources/SimpleRx/BehaviorRelay.swift
@@ -4,24 +4,31 @@ public class BehaviorRelay<Element>: Observable<Element> {
 
     private var observers: [OnNextClosure] = []
     private let lock = NSRecursiveLock()
-    public private(set) var value: Element
+    private var element: Element
 
     init(value: Element) {
-        self.value = value
+        element = value
+    }
+
+    public var value: Element {
+        lock.lock(); defer { lock.unlock() }
+        return element
     }
 
     public func accept(_ element: Element) {
-        lock.lock(); defer { lock.unlock() }
-        value = element
+        lock.lock()
+        self.element = element
         if Thread.isMainThread {
             for o in observers {
                 o(element)
             }
+            lock.unlock()
         } else {
             DispatchQueue.main.async {
                 for o in self.observers {
                     o(element)
                 }
+                self.lock.unlock()
             }
         }
     }
--- Sources/SimpleRx/PublishRelay.swift
+++ Sources/SimpleRx/PublishRelay.swift
@@ -6,16 +6,18 @@ public class PublishRelay<Element>: Observable<Element> {
     private let lock = NSRecursiveLock()
 
     public func accept(_ element: Element) {
-        lock.lock(); defer { lock.unlock() }
+        lock.lock()
         if Thread.isMainThread {
             for o in observers {
                 o(element)
             }
+            lock.unlock()
         } else {
             DispatchQueue.main.async {
                 for o in self.observers {
                     o(element)
                 }
+                self.lock.unlock()
             }
         }
     }
